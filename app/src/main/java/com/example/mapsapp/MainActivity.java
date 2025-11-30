7cfa8dd9a2ecef3eb01553a9e1d17aa55859155a
João Gilberto <gjoao11.dev@gmail.com>
2025-11-30 13:41:39 -0300

    Adiciona tela de configurações

--- app/src/main/java/com/example/mapsapp/MainActivity.java
+++ app/src/main/java/com/example/mapsapp/MainActivity.java
@@ -4,26 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         Button buttonConfig = findViewById(R.id.button_config);
+        Button buttonRegistrarTrilha = findViewById(R.id.button_registrar_trilha);
+        Button buttonVisualizarTrilhas = findViewById(R.id.button_visualizar_trilhas);
+        Button buttonCreditos = findViewById(R.id.button_creditos);
 
         buttonConfig.setOnClickListener(v -> {
             Intent i = new Intent(this, ConfigActivity.class);
